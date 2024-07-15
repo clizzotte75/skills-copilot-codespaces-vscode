b36447ff32afcc6e14d2c405f2ad1ff20ed723a3
clizzotte75 <clizzotte@outlook.com>
2024-07-15 23:11:03 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,35 +1,12 @@
 // create web server
-const express = require('express');
-const app = express();
-const port = 3000;
+var http = require('http');
 
-// middleware
-app.use(express.json());
-
-// endpoint
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+// create web server
+var server = http.createServer(function(req, res) {
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write('<html><body><p>Comments</p></body></html>');
+    res.end();
 });
 
-app.post('/comments', (req, res) => {
-  console.log(req.body);
-  res.send('POST request to the homepage');
-});
-
-// start web server
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
-});
-
-// Run the server, and open your browser to http://localhost:3000/. You should see the text Hello World! on the page. YES
-
-// Now, open another terminal and use curl to send a POST request to the /comments endpoint:
-
-// curl -X POST http://localhost:3000/comments -d '{"comment": "This is a comment!"}' -H 'Content-Type: application/json'
-
-// In the terminal where the server is running, you should see the following output:
-
-// { comment: 'This is a comment!' }
-// This is the comment that was sent in the request body.
-
-// This is a simple example of how to create a web server with Express and handle POST requests. You can use this as a starting point for building more complex applications.
+// listen on port 8080
+server.listen(8080);
